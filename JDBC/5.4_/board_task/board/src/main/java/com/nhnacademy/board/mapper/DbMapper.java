0f1193e39cfdd3a5989d6a53547e668cda32a8f8
김윤민 <ksw08130@naver.com>
2023-05-07 18:00:44 +0900

    upload_dir 방식을 properties 파일에서 관리하도록 변경

---
+++ JDBC/5.4_/board_task/board/src/main/java/com/nhnacademy/board/mapper/DbMapper.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.board.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DbMapper {
+
+}
---
+++ JDBC/5.4_/board_task/board/src/main/java/com/nhnacademy/board/service/UserService.java
@@ -0,0 +1,105 @@
+package com.nhnacademy.board.service;
+
+import com.ctc.wstx.util.StringUtil;
+import com.nhnacademy.board.config.PropertiesConfig;
+import com.nhnacademy.board.domain.User;
+import com.nhnacademy.board.exception.DuplicateStudentIdException;
+import com.nhnacademy.board.exception.StudentNotFoundException;
+import com.nhnacademy.board.repository.UserRepository;
+import com.nhnacademy.board.request.UserRegisterRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    private final PropertiesConfig propertiesConfig;
+
+    public UserService(UserRepository userRepository, PropertiesConfig propertiesConfig) {
+        this.propertiesConfig = propertiesConfig;
+        this.userRepository = userRepository;
+    }
+
+    public User getUser(String id){
+        User user =  userRepository.getUser(id);
+        if(Objects.isNull(user)){
+            throw new StudentNotFoundException(id);
+        }
+        return user;
+    }
+    public void delete(String id){
+        userRepository.remove(id);
+    }
+
+    public List<User> getUserList(){
+        return userRepository.getUsers();
+    }
+    public List<User> getPartList(int num){
+        num = (num-1)*10;
+        int size = 10;
+        List<User> userList = new ArrayList<>();
+        List<User> list = userRepository.getUsers();
+        if(list.size()<num+10){
+            size=list.size()-num;
+        }
+        for(int i=0;i<10;i++){
+            userList.add(list.get(num));
+            num++;
+        }
+        return userList;
+    }
+
+    public void register(UserRegisterRequest userRegisterRequest){
+        MultipartFile file = userRegisterRequest.getProfileFile();
+        if(!file.isEmpty()) {
+            try{
+                file.transferTo(Paths.get(propertiesConfig.getUploadPath() + File.separator + file.getOriginalFilename()));
+            } catch(IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(userRepository.existById(userRegisterRequest.getId())) {
+            throw new DuplicateStudentIdException(userRegisterRequest.getId());
+        }
+        log.info("fileName:{}", userRegisterRequest.getProfileFile().getOriginalFilename());
+        User user  = User.createUser(userRegisterRequest.getId(), userRegisterRequest.getPassword(), userRegisterRequest.getName(), userRegisterRequest.getProfileFileName());
+        userRepository.add(user);
+
+    }
+    public void modify(UserRegisterRequest userRegisterRequest){
+        User user = userRepository.getUser(userRegisterRequest.getId());
+        String uploadPath = propertiesConfig.getUploadPath();
+        MultipartFile file = userRegisterRequest.getProfileFile();
+        if(!file.isEmpty()) {
+            try{
+                file.transferTo(Paths.get(uploadPath + File.separator + file.getOriginalFilename()));
+                user.setProfileFileName(file.getOriginalFilename());
+            } catch(IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        userRepository.modify(user);
+    }
+
+    public String getProfileImagePath(String id) {
+        User user = getUser(id);
+        if (Objects.nonNull(user)) {
+            if(StringUtils.hasText(user.getProfileFileName())){
+                return user.getProfileFileName();
+            }
+        }
+        return "no-image.png";
+    }
+}
