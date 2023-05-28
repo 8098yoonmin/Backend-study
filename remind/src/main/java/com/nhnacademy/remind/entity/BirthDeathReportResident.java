303d7e08b5e49973a8a3218ac031428be4474330
김윤민 <ksw08130@naver.com>
2023-05-28 09:29:11 +0900

    erd 다이어그램을 바탕으로 entity 매핑

---
+++ remind/src/main/java/com/nhnacademy/remind/entity/BirthDeathReportResident.java
@@ -0,0 +1,62 @@
+package com.nhnacademy.remind.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "birth_death_report_resident")
+public class BirthDeathReportResident {
+
+    @EmbeddedId
+    private Pk pk;
+
+    // 출생 or 사망한 사람
+    @MapsId("residentSerialNumber")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "resident_serial_number")
+    private Resident resident;
+
+    // 신고한 사람
+    @MapsId("reportResidentSerialNumber")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "report_resident_serial_number")
+    private Resident reportResidentSerialNumber;
+
+    @Setter
+    @Column(name = "birth_death_report_date")
+    private LocalDate birthDeathReportDate;
+
+    @Column(name = "birth_report_qualifications_code")
+    private String birthReportQualificationsCode;
+
+    @Column(name = "death_report_qualifications_code")
+    private String deathReportQualificationsCode;
+    @Setter
+    @Column(name = "email_address")
+    private String emailAddress;
+    @Setter
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @Getter
+    @Embeddable
+    public static class Pk implements Serializable {
+        @Column(name = "resident_serial_number")
+        private Long residentSerialNumber;
+        @Column(name = "report_resident_serial_number")
+        private Long reportResidentSerialNumber;
+        @Column(name = "birth_death_type_code")
+        private String birthDeathTypeCode;
+    }
+
+}
---
+++ remind/src/main/java/com/nhnacademy/remind/entity/Resident.java
@@ -0,0 +1,44 @@
+package com.nhnacademy.remind.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "resident")
+public class Resident {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "resident_serial_number")
+    private Long residentSerialNumber;
+    @Setter
+    @Column
+    private String name;
+    @Column(name = "resident_id")
+    private String residentId;
+    private String password;
+    @Column(name = "resident_registration_number")
+    private String residentRegistrationNumber;
+    @Column(name = "gender_code")
+    private String genderCode;
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+    @Column(name = "birth_place_code")
+    private String birthPlaceCode;
+    @Setter
+    @Column(name = "registration_base_address")
+    private String registrationBaseAddress;
+    @Column(name = "death_date")
+    private LocalDateTime deathDate;
+    @Column(name = "death_place_code")
+    private String deathPlaceCode;
+    @Column(name = "death_place_address")
+    private String deathPlaceAddress;
+}
