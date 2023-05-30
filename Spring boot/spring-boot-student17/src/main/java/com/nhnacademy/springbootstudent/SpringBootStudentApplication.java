9908dd5e4200ce1b4b206241589644832ee77e0b
김윤민 <ksw08130@naver.com>
2023-05-30 16:05:33 +0900

    main클래스에서 spring boot 실행하기

--- Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
+++ Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
@@ -1,12 +1,19 @@
 package com.nhnacademy.springbootstudent;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootStudentApplication {
+
+	//main클래스에서 실행하는 방법
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootStudentApplication.class, args);
+//		SpringApplication.run(SpringBootStudentApplication.class, args);
+		SpringApplication application = new SpringApplication(SpringBootStudentApplication.class);
+		application.run(args);
 	}
+
 
 }
