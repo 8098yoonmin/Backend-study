a678c38bba7c26a456741572e1fba1b1ecc192b8
김윤민 <ksw08130@naver.com>
2023-05-31 10:37:38 +0900

    sql 의존성 추가

--- Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
+++ Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan //하위에 있는 autoconfiguration을 다 등록해줌
 public class SpringBootStudentApplication {
 
 	//main클래스에서 실행하는 방법
--- Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/configuration/StudentAutoConfiguration.java
+++ Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/configuration/StudentAutoConfiguration.java
@@ -1,2 +1,10 @@
-package com.nhnacademy.springbootstudent.configuration;public class StudentAutoConfiguration {
+package com.nhnacademy.springbootstudent.configuration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableConfigurationProperties(StudentProperties.class)
+public class StudentAutoConfiguration {
+
 }
