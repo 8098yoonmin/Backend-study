65ba48dae46871f9407c007abb49538f509c8caf
김윤민 <ksw08130@naver.com>
2023-05-31 13:22:04 +0900

    싱글톤

--- Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/configuration/StudentAutoConfiguration.java
+++ Spring boot/spring-boot-student17/src/main/java/com/nhnacademy/springbootstudent/configuration/StudentAutoConfiguration.java
@@ -1,10 +1,25 @@
 package com.nhnacademy.springbootstudent.configuration;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 //@EnableConfigurationProperties(StudentProperties.class)
 public class StudentAutoConfiguration {
 
+    @Bean
+    public String a(){
+        return new String(c());
+    }
+
+    @Bean
+    public String b(){
+        return new String(c());
+    }
+    @Bean
+    public String c(){
+        System.out.println("========> c");
+        return new String("c");
+    }
 }
